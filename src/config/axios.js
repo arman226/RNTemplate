899b85fcf830e39c8fc41fb9d782a4cca0df3884
Armando I. Bibay Jr <50895307+arman226@users.noreply.github.com>
2021-03-10 19:48:55 +0800

    Removed unused variables

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
-// import https from 'https';
 
 const REQUEST_TIME_OUT_SECONDS = 30;
-const BASE_URL = 'https://192.168.43.246/TriviaApp/api/';
 
 const BASE_URL_FOR_TESTING = 'https://jsonplaceholder.typicode.com/';
 
